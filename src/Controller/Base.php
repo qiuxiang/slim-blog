1d9514084b1c81f07b9da79c3cb0ae89eef54fb4
Qiu Xiang <i@7c00.cc>
2017-04-26 00:51:54 +0800

    完善文档注释

--- src/Controller/Base.php
+++ src/Controller/Base.php
@@ -1,9 +1,24 @@
 <?php
 
 namespace App\Controller;
+use Psr\Http\Message\ResponseInterface as Response;
+use Slim\Views\Twig;
 
+/**
+ * 基础控制器，主要实现公共方法，身份认证
+ */
 class Base
 {
+    /**
+     * @var Response
+     */
+    protected $response;
+
+    /**
+     * @var Twig
+     */
+    protected $view;
+
     public function __construct($container)
     {
         $this->view = $container->view;
@@ -17,8 +32,17 @@ class Base
             $this->response, $template, array_merge($this->data, $data));
     }
 
+    public function redirect($url)
+    {
+        return $this->response->withStatus(302)->withHeader('Location', $url);
+    }
+
     public function auth($action)
     {
-        return $action();
+        if ($_SESSION['user']) {
+            return $action();
+        } else {
+            return $this->redirect('/login');
+        }
     }
 }
--- src/dependencies.php
+++ src/dependencies.php
@@ -1,14 +1,10 @@
 <?php
-use App\Controller\News as NewsController;
 use Slim\Views\Twig;
 use Slim\Views\TwigExtension;
 
 $container = $app->getContainer();
 $container['view'] = function ($container) {
-    $view = new Twig(__DIR__ . '/../templates/');
+    $view = new Twig('../templates/');
     $view->addExtension(new TwigExtension($container['router'], ''));
     return $view;
-};
-$container[NewsController::class] = function ($container) {
-    return new NewsController($container);
 };
